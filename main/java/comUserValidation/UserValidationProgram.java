d0f243d1859c93883eb68cbb4377774c6b3384c4
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-07 03:55:23 +0530

    ADD: Person contact added through console

--- main/java/comUserValidation/UserValidationProgram.java
+++ main/java/comUserValidation/UserValidationProgram.java
@@ -2,12 +2,64 @@ package comUserValidation;
 
 public class UserValidationProgram
 {
+    //variables
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String email;
+    int zip;
+    long phoneNumber;
+    
+    //method to take inputs from user
+    public void takesInput()
+    {
+        InputFromUser input = new InputFromUser();
+        
+        System.out.println("Enter first name");
+        firstName = input.stringInput();
+        
+        System.out.println("Enter last name");
+        lastName = input.stringInput();
+        
+        System.out.println("Enter address");
+        address = input.stringInput();
+        
+        System.out.println("Enter city");
+        city = input.stringInput();
+        
+        System.out.println("Enter state");
+        state = input.stringInput();
+        
+        System.out.println("Enter email");
+        email = input.stringInput();
+        
+        System.out.println("Enter zip");
+        zip = input.intInput();
+        
+        System.out.println("Enter phone number");
+        phoneNumber = input.longInput();
+    }
+    
     //main method
     public static void main(String[] args)
     {
-        Contact contact = new Contact("Kiran","Kumar","Near house",
-                                        "My cuty","My state","myeamil@gmail.com",
-                                        501526,9586231470L);
-        System.out.println(contact.toString());
+        UserValidationProgram userValidationProgram = new UserValidationProgram();
+        
+        //calling method to take inputs
+        userValidationProgram.takesInput();
+        
+        //calling Contact class
+        Contact contact = new Contact(userValidationProgram.firstName,
+                                        userValidationProgram.lastName,
+                                        userValidationProgram.address,
+                                        userValidationProgram.city,
+                                        userValidationProgram.state,
+                                        userValidationProgram.email,
+                                        userValidationProgram.zip,
+                                        userValidationProgram.phoneNumber);
+        System.out.println(contact);
+        
     }
 }
